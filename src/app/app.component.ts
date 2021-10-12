4dfefc6bbe15adc4ab2bd5238b01cd847106d382
Jefferson-devs <jeffersonsoares.s@yahoo.com>
2021-10-11 22:29:35 -0300

    criando componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,17 @@
+import { TransferenciaService } from './services/transferencia.service';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'bytebank';
-  transferencia: any;
 
+  constructor(private service: TransferenciaService) {}
 
   transferir($event: any) {
-    console.log($event);
-    this.transferencia = $event;
+    this.service.adicionar($event);
   }
 }
